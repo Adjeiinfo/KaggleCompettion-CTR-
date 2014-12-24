4eb9839d3875fa3e2b2d17a08aca4ba90ab4f217
Adjeiinfo <KKouassi2@slb.com>
2014-12-24 18:09:15 +0900

    continue the exploration
    Good to continue

--- kaggleBaseline.py
+++ kaggleBaseline.py
@@ -35,9 +35,9 @@ from math import exp, log, sqrt
 ##############################################################################
 print "Program start"
 # A, paths
-train = 'train.csv'               # path to training file
-test = 'test.csv'                 # path to testing file
-submission = 'submission1234.csv'  # path of to be outputted submission file
+train = '../train.csv'               # path to training file
+test = '../test.csv'                 # path to testing file
+submission = '../submission1234_1.csv'  # path of to be outputted submission file
 
 # B, model
 alpha = .1  # learning rate
@@ -46,13 +46,23 @@ L1 = 1.     # L1 regularization, larger value means more regularized
 L2 = 1.     # L2 regularization, larger value means more regularized
 
 # C, feature/hash trick
-D = 2 ** 24             # number of weights to use
+D = 2 ** 24 
+#D = 1024          # number of weights to use
 interaction = False     # whether to enable poly2 feature interactions
 
 # D, training/validation
 epoch = 1       # learn training data for N passes
 holdafter = 29   # data after date N (exclusive) are used as validation
 holdout = None  # use every N training instance for holdout validation
+
+COLS = "device_ip,device_conn_type,C14,C15,C16,C17,C18,C19,C20,C21"
+mCOLS = "id,click,day,hour,C1,banner_pos,site_id,site_domain,site_category,app_id,app_domain,app_category,device_id,device_ip,device_model,device_type,device_conn_type,C14,C15,C16,C17,C18,C19,C20,C21"
+
+colnames = COLS.split(",")
+
+COL_TO_DROP=""
+DROP_LIST = []
+
 
 ##############################################################################
 # class, function, generator definitions #####################################
@@ -226,7 +236,7 @@ def data(path, D):
                we only need the index since all values are either 0 or 1
             y: y = 1 if we have a click, else we have y = 0
     '''
-
+    global COL_TO_DROP
     for t, row in enumerate(DictReader(open(path))):
         # process id
         ID = row['id']
@@ -244,7 +254,12 @@ def data(path, D):
 
         # turn hour really into hour, it was originally YYMMDDHH
         row['hour'] = row['hour'][6:]
-
+        if COL_TO_DROP != '':
+            del row[COL_TO_DROP]
+        
+        #concat what needs to be (will be enabled soon)
+        ###concatRow(row)
+        
         # build x
         x = []
         for key in row:
@@ -260,19 +275,20 @@ def data(path, D):
 ##############################################################################
 # start training #############################################################
 ##############################################################################
-
-start = datetime.now()
-
-print "program started: ", start
-# initialize ourselves a learner
 learner = ftrl_proximal(alpha, beta, L1, L2, D, interaction)
+def trainAlgo():
+    start = datetime.now()
 
-# start training
-for e in xrange(epoch):
-    loss = 0.
-    count = 0
+    print "program started: ", start
+    # initialize ourselves a learner
+    
 
-    for t, date, ID, x, y in data(train, D):  # data is a generator
+    # start training
+    for e in xrange(epoch):
+        loss = 0.
+        count = 0.
+
+        for t, date, ID, x, y in data(train, D):  # data is a generator
         #    t: just a instance counter
         #    date: you know what this is
         #    ID: id provided in original data
@@ -280,10 +296,10 @@ for e in xrange(epoch):
         #    y: label (click)
 
         # step 1, get prediction from learner
-        p = learner.predict(x)
+            p = learner.predict(x)
         
 
-        if (holdafter and date > holdafter) or (holdout and t % holdout == 0):
+            if (holdafter and date > holdafter) or (holdout and t % holdout == 0):
             # step 2-1, calculate validation loss
             #           we do not train with the validation data so that our
             #           validation loss is an accurate estimation
@@ -292,22 +308,70 @@ for e in xrange(epoch):
             #            validate with instances from day N + 1 and after
             #
             # holdout: validate with every N instance, train with others
-            loss += logloss(p, y)
-            count += 1
-        else:
+                loss += logloss(p, y)
+                count += 1
+            else:
             # step 2-2, update learner with label (click) information
-            learner.update(x, p, y)
+                learner.update(x, p, y)
 
-    print('Epoch %d finished, validation logloss: %f, elapsed time: %s' % (
-        e, loss/count, str(datetime.now() - start)))
-
+        print('Epoch %d finished, validation logloss: %f, elapsed time: %s' % (
+            e, loss/count, str(datetime.now() - start)))
+        return loss/count
 
 ##############################################################################
 # start testing, and build Kaggle's submission file ##########################
 ##############################################################################
+def test():
+    with open(submission, 'w') as outfile:
+        outfile.write('id,click\n')
+        for t, date, ID, x, y in data(test, D):
+            p = learner.predict(x)
+            outfile.write('%s,%s\n' % (ID, str(p)))
 
-with open(submission, 'w') as outfile:
-    outfile.write('id,click\n')
-    for t, date, ID, x, y in data(test, D):
-        p = learner.predict(x)
-        outfile.write('%s,%s\n' % (ID, str(p)))
+def exploreData():
+    global COL_TO_DROP
+    global DROP_LIST
+    
+    #column_impact = []
+    #print colnames
+    #print len(colnames)
+    #first run,no drop 
+    print "Start the data exploration"
+    previousRun = trainAlgo()
+     
+    for col in colnames:
+        COL_TO_DROP = col
+        print "Drop me?:", COL_TO_DROP
+        column_error = trainAlgo()
+        
+        #coldrop has more imp
+        if column_error >= previousRun:
+            DROP_LIST.append(COL_TO_DROP)     
+        print "Colname:", COL_TO_DROP,"contribution:", column_error
+    print column_error
+    print DROP_LIST
+ 
+def concatRow(row):
+    """
+   site_id+site_domain+site_category,
+   app_id,app_domain+app_category,
+   device_id+device_ip+device_model,"
+   """
+    row['site_id'] += row['site_domain'] + row['site_category']
+    del row['site_domain']
+    del row['site_category'] 
+
+    row['app_id'] += row['app_domain'] + row['app_category']
+    del row['app_domain']
+    del row['app_category']
+    
+    row['device_id'] += row['device_model']+ row['device_type'] #+ row['device_conn_type'] + row['device_ip']
+    del row['device_model']
+    del row['device_type']
+    #del row['device_ip']
+    #del row['device_conn_type']
+
+    #return row
+        
+    
+exploreData()
